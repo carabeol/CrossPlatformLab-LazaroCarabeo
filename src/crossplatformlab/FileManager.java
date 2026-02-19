1a60ec268ddca0f4ea1461b06ac2bfc2c61b861f
carabeol <lazaroyovanys@gmail.com>
2026-02-19 17:06:38 -0500

    First commit

---
+++ src/crossplatformlab/FileManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crossplatformlab;
+
+/**
+ *
+ * @author lazaro
+ */
+public class FileManager {
+    
+  int x = 0;
+  int y =10;
+    
+}
